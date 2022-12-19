f56650159d43dddf84df24f06769166f1c19eb15
Aieat Assam <t_l_m_p@hotmail.com>
2022-12-19 23:04:32 +0000

    Now processes files in images directory. Just place png and jpeg files and start it up
    Signed-off-by: Aieat Assam <aieat.assam@epicor.com>

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
+import random
 import signal
 import sys
 import time
 from pprint import pprint
-
+from glob import glob
 import spidev
 import numpy as np
 import ws2812
@@ -17,9 +18,10 @@ HEIGHT = 16
 # uses human indices starting at 1, not array indices!
 SEGMENT_ORDER = [1, 2, 3, 4]
 # how many times we increase saturation by. Power of 2 will make calculation more efficient
-SATURATION_FACTOR = 32
+SATURATION_FACTOR = 1.5
+SATURATION_OFFSET = 0
 # how many times we decrease intensity by. Power of 2 will make calculation more efficient
-INTENSITY_FACTOR = 16
+INTENSITY_FACTOR = 1.5
 PIXELS = WIDTH * HEIGHT
 SPI_DEVICE = 0
 
@@ -61,12 +63,11 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 
-def main():
-    image_file = 'christmas-tree.png'
+def process_file(image_file):
     cache_filename = image_file + '.npy'
     if os.path.isfile(cache_filename):
         # use cached image for speed
-        img = np.load(cache_filename)
+        flattened_matrix = np.load(cache_filename)
     else:
         # no cache, so perform full preprocessing
         img = cv2.imread(image_file)
@@ -76,29 +77,46 @@ def main():
         if w != WIDTH or h != HEIGHT:
             img = cv2.resize(img, dsize=(WIDTH, HEIGHT))
 
-        hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        h, s, v = cv2.split(hsv_img)
-        # raise saturation value
-        s = np.multiply(s, SATURATION_FACTOR)
-        # decrease intensity
-        v = np.divide(v.astype(np.int16), INTENSITY_FACTOR).astype(np.uint8)
-        hsv_img = cv2.merge([h, s, v])
-        img = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2BGR)
+        if SATURATION_FACTOR > 1 or INTENSITY_FACTOR > 1 or SATURATION_OFFSET != 0:
+            hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+            h, s, v = cv2.split(hsv_img)
+            # raise saturation value
+            if SATURATION_FACTOR > 1:
+                s = np.multiply(s.astype(np.int16), SATURATION_FACTOR)
+            if SATURATION_OFFSET != 0:
+                s = np.add(s.astype(np.int16), SATURATION_OFFSET)
+            s = np.clip(s, 0, 255).astype(np.uint8)
+            # decrease intensity
+            if INTENSITY_FACTOR > 1:
+                v = np.divide(v.astype(np.int16), INTENSITY_FACTOR).astype(np.uint8)
+            hsv_img = cv2.merge([h, s, v])
+            img = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2BGR)
 
         # ws2812b takes colour channels as G R B,
         # no native conversion, so we merge them manually
         blue, green, red = cv2.split(img)
         img = cv2.merge([green, red, blue])
-        np.save(cache_filename, img)
+        #np.save(cache_filename, img)
 
-    flattened_matrix = convert_matrix_to_bit_array(img)
+        flattened_matrix = convert_matrix_to_bit_array(img)
+        np.save(cache_filename, flattened_matrix)
+
     target_matrix = flattened_matrix
     ws2812.write2812(spi, target_matrix)
 
 
 if __name__ == '__main__':
-    main()
-    print("Rendering completed. Press CTRL+C to shut down.")
+    # delete old caches to allow regenerating altered saturation and intensity
+    cached_arrays = glob('images/*.npy')
+    for cached in cached_arrays:
+        os.remove(cached)
+
+    files = glob('images/*.png')
+    files.extend(glob('images/*.jpg'))
+    print("Press CTRL+C to shut down.")
 
     while True:
-        time.sleep(10)
+        random.shuffle(files)
+        for file in files:
+            process_file(file)
+            time.sleep(3)
